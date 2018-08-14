43655cf593a168d02c52a5f46b1ac6b869ace9a2
vaibhavbuccha <jainvaibhav415@gmail.com>
2018-08-14 19:12:03 +0530

    Add files via upload

---
+++ html/signup.php
@@ -0,0 +1,10 @@
+<?php
+$a=$_POST["user"];
+$b=$_POST["fname"];
+$c=$_POST["mail"];
+$d=$_POST["password"];
+echo "username: $a   ";
+echo "fullname: $b   ";
+echo "mail: $c     ";
+echo "password: $d    ";
+?>
